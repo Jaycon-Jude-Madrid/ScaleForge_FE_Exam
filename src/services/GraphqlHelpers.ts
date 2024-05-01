f65eec4f74d49ae12080d42af9d3a1afb172b991
Jaycon Madrid <jaycon46madrid@gmail.com>
2024-05-01 14:57:43 +0800

    fix

--- src/services/GraphqlHelpers.ts
+++ src/services/GraphqlHelpers.ts
@@ -8,6 +8,10 @@ declare var process: {
 	};
 };
 
+//I didnt use the ENV it might crash the app since it will be ignored in the git
+const URL = "https://report.development.opexa.io/graphql";
+const TOKEN =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjakp0ZFdQaGhkUHlYU25SdSIsInJvbGUiOiJBRE1JTiIsImp0aSI6ImE4MjFlYjM1Y2NmZjI0NjAwNjI0ZGFjYSIsImlwQWRkcmVzcyI6IjE0My40NC4xOTIuMTA3IiwibG9jYXRpb24iOiJDYWdheWFuIGRlIE9ybywgUGhpbGlwcGluZXMiLCJwbGF0Zm9ybSI6IjEydXd1UkNjWXAxY1dpWHpQWSIsImlhcCI6IjIwMjQtMDQtMjRUMDA6MTc6MjAuMDI4KzAwOjAwIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTcxMzkxNzg3MCwiZXhwIjoxNzc2OTg5ODcwfQ.UfB36fjFrYvg8TV9VYEtNfG6CzRlz9pnjKnqfru-1Hc";
 export async function fetchMembersData({
 	first,
 	after,
@@ -16,9 +20,9 @@ export async function fetchMembersData({
 	after?: string;
 }): Promise<MembersData> {
 	try {
-		const client = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL, {
+		const client = new GraphQLClient(URL, {
 			headers: {
-				Authorization: `Bearer ${process.env.NEXT_PUBLIC_HEADERS_TOKEN}`,
+				Authorization: `Bearer ${TOKEN}`,
 			},
 		});
 
@@ -54,12 +58,11 @@ export async function fetchMembersData({
 }
 
 export async function getQueryName(variables: { search: string }) {
-	const client = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL, {
+	const client = new GraphQLClient(URL, {
 		headers: {
-			Authorization: `Bearer ${process.env.NEXT_PUBLIC_HEADERS_TOKEN}`,
+			Authorization: `Bearer ${TOKEN}`,
 		},
 	});
-
 	const query = `
       query ($search: String!) {
         membersByName(search: $search, first: 20) {
